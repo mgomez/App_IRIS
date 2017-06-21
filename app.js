3141a781b02ce09d07c9dd4e0e8af559e1541a90
Cesar Gomez <00.13.zkr@gmail.com>
2017-06-20 23:47:09 -0500

    init

---
+++ app.js
@@ -0,0 +1,43 @@
+var connectionStatus;
+var networkState;
+
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+  window.open = cordova.InAppBrowser.open;
+  document.addEventListener("offline", onOffline, false);
+  document.addEventListener("online", online, false);
+  networkState = navigator.connection.type;
+
+  if (navigator.notification) {
+    window.alert = function (message) {
+      navigator.notification.alert(
+        message,
+        null,
+        "IRIS",
+        'OK'
+      );
+    };
+  }
+  if (networkState != 'none') {
+    var win = window.open("http://kinderiris.com/", '_self', 'location=yes');
+  } else {
+    navigator.notification.alert(
+      'No esta conectado a una red Wifi',
+      function () {
+        navigator.app.exitApp();
+      },
+      'IRIS',
+      'Ok'
+    );
+  }
+}
+
+function online() {
+  connectionStatus = true;
+}
+
+function onOffline() {
+  connectionStatus = false;
+  alert("Se perdio la conexion a internet.");
+}
